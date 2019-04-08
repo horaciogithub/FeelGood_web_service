4a0395b9880a0e46694950215b66ce2f7fa6eae3
Horacio Ramírez <horacioram94@gmail.com>
2019-04-08 20:17:32 +0100

    El servicio devuelve el token y los datos del usuario que se ha logeado

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -23,11 +23,22 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
 
+    // public function login(){
+    //     if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+    //         $user = Auth::user();
+    //         $success['token'] =  $user->createToken('MyApp')->accessToken;
+    //         return response()->json(['success' => $success], $this->successStatus);
+    //     }
+    //     else{
+    //         return response()->json(['error'=>'Unauthorised'], 401);
+    //     }
+    // }
+
     public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('MyApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
+            return response()->json(['userData' => $user, 'success' => $success], $this->successStatus);
         }
         else{
             return response()->json(['error'=>'Unauthorised'], 401);
